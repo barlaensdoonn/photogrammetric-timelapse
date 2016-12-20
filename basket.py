5c205d440301c6a2dd6cb5ff26d9a1316fa3263b
barlaensdoonn <brandon.aleson@gmail.com>
2016-12-19 22:35:36 -0800

    wrap code in functions

--- basket.py
+++ basket.py
@@ -9,22 +9,37 @@ import sys
 MCAST_GRP = '225.1.1.1'
 MCAST_PORT = 3179
 
-print("Sending shooting command...")
 
-dev = "eth0" + "\0"
-SCMD = chr(1)  # Command 1 = Shoot photo
-
-if len(sys.argv) < 2:
-    print("please provide raspistill command options")
-
-else:
-    options = ""
-    for a in range(1, len(sys.argv)):
-        options = options + " " + sys.argv[a]
-    SCMD = SCMD + options
-    SEND = SCMD.encode()
+def config_socket():
+    dev = "eth0" + "\0"
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, dev.encode())
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-    sock.sendto(SEND, (MCAST_GRP, MCAST_PORT))
-    sock.close()
+
+    return sock
+
+
+def get_options():
+    options = ""
+    SCMD = chr(1)  # Command 1 = Shoot photo
+
+    for a in range(1, len(sys.argv)):
+        options = options + " " + sys.argv[a]
+    SCMD = SCMD + options
+    send = SCMD.encode()
+
+    return send
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("please provide raspistill command options")
+
+    else:
+        print("Sending shooting command...")
+
+        sock = config_socket()
+        send = get_options()
+
+        sock.sendto(send, (MCAST_GRP, MCAST_PORT))
+        sock.close()
