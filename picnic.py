658b97b183787bd497851566e323494daf58957c
barlaensdoonn <brandon.aleson@gmail.com>
2016-12-19 22:25:30 -0800

    wrap socket config in function

--- picnic.py
+++ picnic.py
@@ -7,25 +7,31 @@ import struct
 import subprocess
 
 
-MCAST_GRP = '225.1.1.1'
-MCAST_PORT = 3179
+def config_socket():
+    MCAST_GRP = '225.1.1.1'
+    MCAST_PORT = 3179
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    return sock
 
 debug = 1  # Turn debug message on/off
 
-while True:
-    data = sock.recv(10240).decode()
-    rdata = data[1:]
-    rcmd = ord(data[0])
-    if debug == 1:
-        print("Received cmd: {}".format(str(rcmd)))
-        print("Data: {}".format(rdata))
-    if (rcmd == 1):
-        print("shooting")
-        cmd = "raspistill " + rdata
-        pid = subprocess.call(cmd, shell=True)
+if __name__ == '__main__':
+    sock = config_socket()
+
+    while True:
+        data = sock.recv(10240).decode()
+        rdata = data[1:]
+        rcmd = ord(data[0])
+        if debug == 1:
+            print("Received cmd: {}".format(str(rcmd)))
+            print("Data: {}".format(rdata))
+        if (rcmd == 1):
+            print("shooting")
+            cmd = "raspistill " + rdata
+            pid = subprocess.call(cmd, shell=True)
