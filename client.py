0b92595fa720981fbac3c05bb28207477cdf00d8
barlaensdoonn <brandon.aleson@gmail.com>
2018-08-27 16:39:50 -0700

    cam

--- client.py
+++ client.py
@@ -1,49 +1,35 @@
 #!/usr/bin/python3
 # process trigger command passed over MQTT
 # 8/25/18
-# updated 8/25/18
+# updated 8/27/18
 
 import paho.mqtt.client as mqtt
 
 
-broker = 'photogram00.local'
-port = 1883
-keepalive = 60
-topic = 'test'
-qos = 2
+class MQTTCam(mqtt.Client):
 
-
-class MQTTClientReference(mqtt.Client):
-    '''
-    original client class from paho.mqtt.python examples. more here:
-    http://www.eclipse.org/paho/clients/python/
-    '''
-
-    def on_connect(self, mqttc, obj, flags, rc):
-        print('response code: {}'.format(str(rc)))
+    def __init__(self, broker='mqtt-broker.local', port=1883, topic='', qos=0, keepalive=60, *args, **kwargs):
+        self.broker = broker
+        self.port = port
+        self.topic = topic
+        self.qos = qos
+        self.keepalive = keepalive
+        mqtt.Client.__init__(self, *args, **kwargs)
 
     def on_message(self, mqttc, obj, msg):
-        print('topic: {}   QOS: {}   payload: {}'.format(msg.topic, str(msg.qos), str(msg.payload)))
-
-    def on_publish(self, mqttc, obj, mid):
-        print('mid: {}'.format(str(mid)))
-
-    def on_subscribe(self, mqttc, obj, mid, granted_qos):
-        print('subscribed: {}   granted_QOS: {}'.format(str(mid), str(granted_qos)))
-
-    def on_log(self, mqttc, obj, level, string):
-        print('log: {}'.format(string))
+        payload = msg.payload.decode()
+        print('topic: {}   QOS: {}   payload: {}'.format(msg.topic, str(msg.qos), payload))
 
     def run(self):
-        self.connect(broker, port, keepalive)
-        self.subscribe(topic, qos)
+        self.connect(self.host, self.port, self.keepalive)
+        self.subscribe(self.topic, self.qos)
 
-        rc = 0
-        while rc == 0:
-            rc = self.loop()
-        return rc
+        response_code = 0
+        while response_code == 0:
+            response_code = self.loop()
+        return response_code
 
 
 if __name__ == '__main__':
-    client = MQTTClientReference()
+    client = MQTTCam(broker='photogram00.local', topic='shutter', qos=2)
     client.run()
--- server.py
+++ server.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 # publish trigger command to MQTT
 # 8/25/18
-# updated 8/25/18
+# updated 8/27/18
 
-import paho.mqtt.client as mqtt
-
-
-broker = 'photogram00.local'
-port = 1883
-keepalive = 60
-topic = 'test'
-qos = 2
+import paho.mqtt.publish as publish
 
 
 if __name__ == '__main__':
-    client = mqtt.Client()
-    client.connect_async(broker, port, keepalive)
-    client.loop_start()
-
-    msg = 'testing'
-    client.publish(topic, payload=msg, qos=qos)
+    broker = 'photogram00.local'
+    topic = 'shutter'
+    qos = 2
+    msg = 1
+    publish.single(topic, msg, hostname=broker, qos=qos)
